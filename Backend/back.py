89625b290ac28dd1a06781be0cfb8c7b73b5791d
szagyarorszag <bakitzhana566@gmail.com>
2024-10-19 13:52:45 +0500

    front and back py files has been uplanted

--- Backend/back.py
+++ Backend/back.py
@@ -2,11 +2,10 @@ import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 
-
 def generate_response(prompt: str):
     load_dotenv()
     genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
     model = genai.GenerativeModel(model_name="gemini-1.5-flash")
     response = model.generate_content(prompt)
-    print(response.text)
+    return response.text
--- Frontend/front.py
+++ Frontend/front.py
@@ -1,23 +1,37 @@
 import streamlit as st
 import requests
+import sys
+import os
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import Backend.back as back
+
+# Основной интерфейс
 st.title("Text")
 
+# Поле для ввода текста
 text_input = st.text_area("Введите текст")
 
+# Кнопка для загрузки файла
 uploaded_file = st.file_uploader("Прикрепите файл", type=["txt", "pdf", "docx"])
 
+# Кнопка для отправки данных
 if st.button("Отправить"):
     if text_input:
-        endpoint_url = "http://your-backend-endpoint.com/generate"
-        response = requests.post(endpoint_url, json={"text": text_input})
-        if response.status_code == 200:
-            st.write("Ответ на введенный текст:")
-            st.write(response.json().get("response_text", "Ошибка в ответе от сервера."))
-        else:
-            st.error("Произошла ошибка при обращении к серверу. Попробуйте позже.")
-    else:
-        st.warning("Пожалуйста, введите текст перед отправкой.")
+        response = back.generate_response(text_input)
+        st.write(response)
+        print(response)
+        # Отправка текста на endpoint для генерации ответа
+    #     endpoint_url = "http://your-backend-endpoint.com/generate"  # Замените на фактический URL вашего endpoint
+    #     response = requests.post(endpoint_url, json={"text": text_input})
+    #     if response.status_code == 200:
+    #         st.write("Ответ на введенный текст:")
+    #         response_text = response.json().get("response_text", "Ошибка в ответе от сервера.")
+    #         st.markdown(response_text)  # Отображение ответа в формате Markdown
+    #     else:
+    #         st.error("Произошла ошибка при обращении к серверу. Попробуйте позже.")
+    # else:
+    #     st.warning("Пожалуйста, введите текст перед отправкой.")
 
     if uploaded_file is not None:
         st.write("Загруженный файл:")
@@ -25,4 +39,3 @@ if st.button("Отправить"):
     else:
         st.warning("Пожалуйста, загрузите файл перед отправкой.")
 
-st.text_input("Input Text", "Введите ваш текст здесь")
